26443f088bc756794853b727d14d008313538375
yuichisan <yuuichi_20n2100080@nnn.ed.jp>
2021-02-03 01:10:27 +0900

    脆弱性対策とデザイン改善

--- test.js
+++ test.js
@@ -8,7 +8,7 @@ const html = pug.renderFile('./views/posts.pug', {
       id: 1,
       content: "<script>alert('test');</script>",
       postedBy: 'guest1',
-      trackingCookie: 1,
+      trackingCookie: '1234567812345678_bb92126be15c1995c1d3d03db910a69e1521129dd70fae16552d6d9cb419584e05d6818e8ca43901',
       createdAt: new Date(),
       updatedAt: new Date()
     }
